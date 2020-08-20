2639d528430b04bc6004eba455568a722d1a64a6
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-08-20 16:15:36 -0600

    Improve clarity of `histogram.asm`

--- asm/histogram.asm
+++ asm/histogram.asm
@@ -5,7 +5,8 @@ LINES:
     LDI R2, MAX_ASTERISKS 
     LD R2, R2
     CMP R0, R2
-    
+
+    ; If the number is greater than MAX_ASTERISKS, stop
     LDI R1, END
     JGT R1
     
@@ -18,7 +19,10 @@ LINES:
     LDI R2, ASTERISK
     LD R2, R2
 ASTERISKS:
+    ; Print asterisk from R2
     PRA R2
+
+    ; Loop while number of asterisks to print (R3) is greater than 0
     DEC R3
     CMP R3, R4
     JGT R1
@@ -27,13 +31,13 @@ ASTERISKS:
     LDI R2, 2
     MUL R0, R2
     
-    LDI R1, LINES
-
     ; Print newline
     LDI R2, NEWLINE
     LD R2, R2
     PRA R2
-    
+
+    ; Jump to LINES
+    LDI R1, LINES
     JMP R1
 
 END:
