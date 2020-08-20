1c0b9343ab22f54c9cc14210bfd973f1a492b79f
leroycep <leroycepearson@geemili.xyz>
2020-08-20 15:31:24 +0000

    Deploying to gh-pages from  @ 5d0c9afc48e61c970b685ca44f3f405ecf94e807 🚀

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ let utf8_text_decoder = new TextDecoder();
 let next_buffer_id = 1;
 let wasm_owned_buffers = {};
 wasm_owned_buffers[0] = new Uint8Array();
+let keyboard_input_queue = [];
 
 const env = {
   get_output_buffer: () => 0,
@@ -25,13 +26,17 @@ const env = {
     const wasm_array = new Uint8Array(instance.exports.memory.buffer, ptr, len);
     const prev_array = wasm_owned_buffers[buffer_id];
 
-    wasm_owned_buffers[buffer_id] = new Uint8Array(wasm_array.length + prev_array.length);
+    wasm_owned_buffers[buffer_id] = new Uint8Array(
+      wasm_array.length + prev_array.length
+    );
     wasm_owned_buffers[buffer_id].set(prev_array, 0);
     wasm_owned_buffers[buffer_id].set(wasm_array, prev_array.length);
   },
   buffer_deinit: (buffer_id) => {
     delete wasm_owned_buffers[buffer_id];
   },
+  keyboard_input_empty: () => keyboard_input_queue.length === 0,
+  keyboard_input_read_byte: () => keyboard_input_queue.shift(),
   console_error: (buffer_id) => {
     console.error(utf8_text_decoder.decode(wasm_owned_buffers[buffer_id]));
   },
@@ -47,12 +52,10 @@ const env = {
   get_time_seconds: () => new Date().getTime(),
 };
 
-WebAssembly.instantiateStreaming(fetch(WASM_PATH), { env }).then(
-  (obj) => {
-    instance = obj.instance;
-    instance.exports.init();
-  }
-);
+WebAssembly.instantiateStreaming(fetch(WASM_PATH), { env }).then((obj) => {
+  instance = obj.instance;
+  instance.exports.init();
+});
 
 // A flag that let's us know if the CPU has started execution of the code
 let run_interval = null;
@@ -69,6 +72,7 @@ function toggleRunning() {
   } else {
     instance.exports.reset();
     wasm_owned_buffers[env.get_output_buffer()] = new Uint8Array();
+    keyboard_input_queue = [];
 
     const asm = utf8_text_encoder.encode(editor.getValue());
     const wasm_array_ptr = instance.exports.malloc(asm.length);
@@ -101,3 +105,43 @@ function stepMany() {
   const text = utf8_text_decoder.decode(wasm_owned_buffers[0]);
   output.textContent = text;
 }
+
+output.addEventListener("keydown", (ev) => {
+  if (!ev.composing) {
+    switch (ev.key) {
+      case "Unidentified":
+      case "Alt":
+      case "AltGraph":
+      case "CapsLock":
+      case "Control":
+      case "Fn":
+      case "FnLock":
+      case "Hyper":
+      case "Meta":
+      case "NumLock":
+      case "ScrollLock":
+      case "Shift":
+      case "Super":
+      case "Symbol":
+      case "SymbolLock":
+      case "Enter":
+      case "Tab":
+      case "ArrowDown":
+      case "ArrowLeft":
+      case "ArrowRight":
+      case "ArrowUp":
+      case "OS":
+      case "Escape":
+      case "Backspace":
+        // Don't send text input events for special keys
+        return;
+      default:
+        break;
+    }
+
+    let bytes = utf8_text_encoder.encode(ev.key);
+    for (b of bytes) {
+      keyboard_input_queue.push(b);
+    }
+  }
+});
